e1debad18ef6d39f9a3de2d07789c87b43724342
OliverLeitner <never@neverslair-blog.net>
2013-11-18 10:10:27 +0100

    fix: projekktor stuttering audio, better video quality, cpu usage reduction

--- cmd/start.php
+++ cmd/start.php
@@ -1,6 +1,6 @@
 <?php
 //enable all error output weve got...
-error_reporting("E_ALL");
+error_reporting(E_ALL);
 ini_set("display_errors","on");
 
 //this receives the var from the call...
@@ -12,18 +12,19 @@ $name=$_GET['name'];
 $uid=md5($_SERVER['REMOTE_ADDR'].$name); //multi user stuff...
 
 $source="avconv -re -i '{$file}'";
-$target="tcp://127.0.0.1:6666?pkt_size=1613";
+$target="tcp://127.0.0.1:6666?pkt_size=650"; //1613
 
 //$presets = "-threads 2";
 $presets = "";
 
 //$audio="-acodec libfaac -ab 12k -aq 6 -ar 48000 -ac 1 -async 2";
-$audio="-acodec aac -ab 12k -aq 6 -ar 48000 -ac 1";
-
+//$audio="-acodec aac -ab 8k -aq 6 -ar 24000 -ac 1";
+$audio = "-acodec libmp3lame -ab 8k -ar 22050 -aq 24 -ac 1"; //-ar 11025
 //$video="-vcodec libx264 -intra -bufsize {$buffer}k -maxrate {$buffer}k -minrate 200k -bf 10 -s {$width}:{$height} -vf scale={$width}:{$height} -vbsf h264_mp4toannexb";
-$video="-vcodec flv -b:v {$buffer}k -s {$width}x{$height} -strict experimental -g 25 -me_method zero";
+//$video="-vcodec flv -b:v 100k -s vga -strict experimental -g 10 -me_method zero";
+$video="-vcodec libx264 -pass 1 -b:v 200k -s vga -strict experimental -g 20 -me_method zero";
 
-$output = "-f flv -metadata streamName=".$uid;
+$output = "-f flv -r 30 -metadata streamName=".$uid;
 
 $check_cmd = "ps auxf |grep {$uid} |awk '{ print $13}' |grep avconv";
 $checked = exec($check_cmd);
