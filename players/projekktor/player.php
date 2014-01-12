4df5255485557ec56d8737263fd30ec67b8ec49d
OliverLeitner <never@neverslair-blog.net>
2014-01-12 21:52:06 +0100

    modifications for projekktor

--- players/projekktor/player.php
+++ players/projekktor/player.php
@@ -3,7 +3,7 @@ ini_set("display_errors","Off");
 
 /* projekktor definitions */
 $style = "<link rel=\"stylesheet\" href=\"".$js_dir."projekktor/themes/maccaco/projekktor.style.css\" type=\"text/css\" media=\"screen\" />";
-$headscript = "<script type=\"text/javascript\" src=\"".$js_dir."projekktor/projekktor-1.3.01.min.js\"></script>";
+$headscript = "<script type=\"text/javascript\" src=\"".$js_dir."projekktor/projekktor-1.3.08.min.js\"></script>";
 $contentscript .= "<script type=\"text/javascript\">
 	$(document).ready(function() {
 		projekktor('#container1', {
