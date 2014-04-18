1f3d284bb4b649a889d217eb9c70db0c488cd6ee
OliverLeitner <never@neverslair-blog.net>
2014-04-19 00:07:00 +0200

    projekktor version update

--- players/projekktor/player.php
+++ players/projekktor/player.php
@@ -4,7 +4,7 @@ ini_set("display_errors","Off");
 /* projekktor definitions */
 $quotes = array("'", '"');
 $style = "<link rel=\"stylesheet\" href=\"".$js_dir."projekktor/themes/maccaco/projekktor.style.css\" type=\"text/css\" media=\"screen\" />";
-$headscript = "<script type=\"text/javascript\" src=\"".$js_dir."projekktor/projekktor-1.3.08.min.js\"></script>";
+$headscript = "<script type=\"text/javascript\" src=\"".$js_dir."projekktor/projekktor-1.3.09.min.js\"></script>";
 $contentscript .= "<script type=\"text/javascript\">
 	$(document).ready(function() {
 		projekktor('#container1', {
@@ -14,10 +14,11 @@ $contentscript .= "<script type=\"text/javascript\">
 			useYTIframeAPI: 'true',
 			platforms: ['flash', 'native', 'vlc', 'browser', 'android', 'ios'],
 			title: '".htmlentities(str_replace($quotes, "", $name_cmd))."',
-			controls: 'true',
+            controls: 'true',
+            duration: ".$seconds.",
 			playlist: [{
                 0:{src:'".htmlentities(str_replace($quotes, "", $default_src))."',type:'".htmlentities(str_replace($quotes, "", $type))."'},
-				config:{streamType:'rtmp', streamServer:'rtmp://".$crtmpserver."/live'}
+				config:{streamType:'rtmp', streamServer:'rtmp://".$crtmpserver."/vod', duration: ".$seconds."}
 			}]
 		});
 	});
